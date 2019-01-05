c15e2baa59bb9e60a3b957dfeceb875646b192bb
Michael Ort <michaelort33@gmail.com>
2019-01-05 06:48:21 -0500

    visualize the graph. creating massive list of all paths from root to leaf.

--- sql_vsualize.py
+++ sql_vsualize.py
@@ -7,6 +7,8 @@ This is a temporary script file.
 from sqlalchemy import create_engine
 import pandas as pd
 import networkx as nx
+import numpy as np
+
 
 
 engine = create_engine("mysql://root:@127.0.0.1/gre_genome")
@@ -17,7 +19,7 @@ edge = pd.read_sql('SELECT * FROM edge', con=my_con)
 
 ###
 
-G = nx.Graph()
+G = nx.DiGraph()
 
 graph_edges=edge[['Start Vertex','End Vertex']]
 tuple_edges=[tuple(x) for x in graph_edges.values]
@@ -28,8 +30,32 @@ tuple_nodes=[tuple(x) for x in graph_nodes.values]
 G.add_edges_from(tuple_edges)
 G.add_nodes_from(tuple_nodes)
 
-nx.draw_networkx(G)
+leaf_nodes = [node for node in G if G.degree(node)==1]
 
+root_nodes = list(set(edge['Start Vertex']
+                [~edge.loc[:,'Start Vertex'].isin(edge.loc[:,'End Vertex'])]))
+
+paths=[]
+for leaf in leaf_nodes:
+    for root in root_nodes:
+        paths.append(list(nx.all_simple_paths(G,root,leaf)))
+        
+paths=[item for item in paths if len(item) > 0]
+paths = [item for sublist in paths for item in sublist]
+
+            
+def convert_node_to_name(list_of_paths,node):
+    main_list=[]
+    for each_list in list_of_paths:
+        new_list=[]
+        for each_node in each_list:
+           new_list.append(node[node.loc[:,'deID']==each_node]['Node'].to_string(index=False,header=False))
+        main_list.append(new_list)
+    return main_list
+
+test=convert_node_to_name(paths,node)
+    
+    
 
 #node.to_sql(con=engine, if_exists='replace', index=False, name='node')
 #edge.to_sql(con=engine, if_exists='replace', index=False, name='edge')
