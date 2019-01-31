8a9849cfa64fb3666aeec359f3e9d259bbf006d5
Michael Ort <michaelort33@gmail.com>
2019-01-31 15:12:12 -0500

    no idea but i didn't update in awhile

--- sql_read_write.py
+++ sql_read_write.py
@@ -1,38 +1,40 @@
-# -*- coding: utf-8 -*-
 """
 Spyder Editor
 
 This is a temporary script file.
 """
+#%%
 from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
 import edit_graph as eg
 import networkx as nx
 from collections import Counter
+import os
 
 
-#Read from data base
+#%%Read from data base
+os.chdir('/home/michael/GREGenome/Python')
 
 #remote
-#remote_engine = create_engine("mysql://gregen5_root:Mooose33@gregenome.com/gregen5_gre_questions")
-#my_con_remote=remote_engine.connect()
+remote_engine = create_engine("mysql+pymysql://gregen5_root:Mooose33@gregenome.com/gregen5_gre_questions")
+my_con_remote=remote_engine.connect()
 
 #local
-local_engine = create_engine("mysql://root:@127.0.0.1/gre_genome")
+local_engine = create_engine("mysql+pymysql://root:@127.0.0.1/gre_genome")
 my_con_local=local_engine.connect()
 
 
-node = pd.read_sql('SELECT * FROM node', con=my_con_local)
-edge = pd.read_sql('SELECT * FROM edge', con=my_con_local)
-quiz = pd.read_sql('SELECT * FROM quiz', con=my_con_local)
-join_table = pd.read_sql('SELECT * FROM join_table', con=my_con_local)
-users = pd.read_sql('SELECT * FROM users', con=my_con_local)
+node = pd.read_sql('SELECT * FROM node', con=local_engine)
+edge = pd.read_sql('SELECT * FROM edge', con=local_engine)
+quiz = pd.read_sql('SELECT * FROM quiz', con=local_engine)
+join_table = pd.read_sql('SELECT * FROM join_table', con=local_engine)
+users = pd.read_sql('SELECT * FROM users', con=local_engine)
 
 
 #Write
-node.to_sql(con=local_engine, if_exists='replace', index=False, name='node')
-edge.to_sql(con=local_engine, if_exists='replace', index=False, name='edge')
+#node.to_sql(con=local_engine, if_exists='replace', index=False, name='node')
+#edge.to_sql(con=local_engine, if_exists='replace', index=False, name='edge')
 #join_table.to_sql(con=local_engine, if_exists='replace', index=False, name='join_table')
 #quiz.to_sql(con=local_engine, if_exists='replace', index=False, name='quiz')
 #users.to_sql(con=local_engine, if_exists='replace', index=False, name='users')
@@ -43,5 +45,11 @@ edge.to_sql(con=local_engine, if_exists='replace', index=False, name='edge')
 #join_table.to_pickle("./join_table.pkl")
 #quiz.to_pickle("./quiz.pkl")
 #users.to_pickle("./users.pkl")
+
+#node = pd.read_pickle("./node.pkl")
+#edge = pd.read_pickle("./edge.pkl")
+#quiz = pd.read_pickle("./quiz.pkl")
+#join_table = pd.read_pickle("./join_table.pkl")
+#users = pd.read_pickle("./users.pkl")
 
 #analyze which magoosh i have already:
